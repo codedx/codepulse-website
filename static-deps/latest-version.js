89126bb8d6ea3b6ada1a9f50fd9858cc77455868
Steve Salas <stephen.salas@securedecisions.com>
2018-04-13 20:07:18 -0400

    Uses codedx.com contact email address

--- static-deps/latest-version.js
+++ static-deps/latest-version.js
@@ -1,7 +1,7 @@
 /* Ask the Github API for the latest codepulse release, and set any `.version`
  * element's text to the "name" of that release.
  */
-$.ajax('https://api.github.com/repos/secdec/codepulse/releases/latest')
+$.ajax('https://api.github.com/repos/codedx/codepulse/releases/latest')
 	.fail(function(err){
 		console.log('automatic "latest version" check failed', err)
 		$(function(){
